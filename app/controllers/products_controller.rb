3ff05380ffd256b31465f85aee4555176c995ae5
Scott Wachtmann <swachtmann@sazboom.com>
2012-11-01 15:06:07 -0400

    Add comment to products#show
    Summary: Add comment line at top of show controller for Products
    
    Test Plan: None
    
    Reviewers: aaron
    
    Reviewed By: aaron
    
    Differential Revision: http://phabricator.sazboom.com/D4

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,6 +13,7 @@ class ProductsController < ApplicationController
   # GET /products/1
   # GET /products/1.json
   def show
+    #This comment is for approving changes
     @product = Product.find(params[:id])
 
     respond_to do |format|
