34b9f031e3e2c4c59500194400877a4b2714708e
Scott Wachtmann <swachtmann@sazboom.com>
2012-11-05 11:32:54 -0500

    add functional tests to store#index controller
    Summary: added functional tests for proper rendering of the store#index view.
    
    Test Plan: N/A
    
    Reviewers: aaron
    
    Reviewed By: aaron
    
    Differential Revision: http://phabricator.sazboom.com/D9

--- test/functional/store_controller_test.rb
+++ test/functional/store_controller_test.rb
@@ -3,7 +3,26 @@ require 'test_helper'
 class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
-    assert_response :success
+    assert_response :success    
   end
 
+  test "index should have at least 4 links" do
+    get :index  
+    assert_select '#columns #side a', minimum: 4
+  end
+  
+  test "index should have at least 3 products displayed" do
+    get :index  
+    assert_select '#main .entry', 3
+  end
+  
+  test "index should display book titles as h3" do
+    get :index  
+    assert_select 'h3', 'Programming Ruby 1.9'
+  end
+  
+  test "index should display price in currency format" do
+    get :index  
+    assert_select '.price', /\$[,\d]+\.\d\d/
+  end
 end
