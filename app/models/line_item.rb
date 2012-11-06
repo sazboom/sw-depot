d7184ec0e71da247866ceaaf485312ba48785ef2
Scott Wachtmann <swachtmann@sazboom.com>
2012-11-06 11:23:38 -0500

    Add ability to clear cart
    Summary: modify cart functionality to include the ability to clear the contents.  Also updated database to allow multiple quantities of the same item in the cart on a single line item.
    
    Test Plan: N/A
    
    Reviewers: aaron
    
    Reviewed By: aaron
    
    Differential Revision: http://phabricator.sazboom.com/D12

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,4 +2,8 @@ class LineItem < ActiveRecord::Base
   belongs_to :cart
   belongs_to :product
   attr_accessible :cart_id, :product_id
+  
+  def total_price
+    product.price * quantity
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Depot::Application.routes.draw do
   resources :line_items
-
   resources :carts
 
   root :to => 'store#index'
